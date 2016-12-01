f8a967ad94199fab11129996452c8198f3f4dba9
Darhakexge <darhakexge@gmail.com>
2016-11-30 20:59:25 -0500

    Migración de vistas

--- web/auth/js/checkBrowser.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	if (!(window.CSS && window.CSS.supports && window.CSS.supports("( display: grid ) and ( --var: grid ) and ( display: var(--var) )" ))){
-		if(decodeURI(location.pathname)!="/browserUpdate.html"){
-			window.location="/browserUpdate.html";
-		}
-	} else if(decodeURI(location.pathname)=="/browserUpdate.html"){
-		window.location="/auth/";
-	}
-});
--- web/js/cambioContrasena.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-	var cambioContrasena = document.querySelectorAll('#contrasena-panel input');
-	cambioContrasena[0].addEventListener("change", function () {
-		for(var i = 1; i < cambioContrasena.length; i++){
-			(cambioContrasena[i].getAttribute("required"))? cambioContrasena[i].removeAttribute("required") : cambioContrasena[i].setAttribute("required", "true");
-		}
-	});
-});
