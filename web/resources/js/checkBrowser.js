206793bfcae3bd7aa6ae4d836a6d20accc60106a
Darhakexge <darhakexge@gmail.com>
2016-12-10 20:38:03 -0500

    Agregadas las páginas de error

--- web/resources/js/checkBrowser.js
+++ web/resources/js/checkBrowser.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 	if (!(window.CSS && window.CSS.supports && window.CSS.supports("( display: grid ) and ( --var: grid ) and ( display: var(--var) )" ))){
-		if(decodeURI(location.pathname)!="/browserUpdate.html"){
+		if(decodeURI(location.pathname)!="/browserUpdate.xhtml"){
 			window.location="/browserUpdate.html";
 		}
-	} else if(decodeURI(location.pathname)=="/browserUpdate.html"){
+	} else if(decodeURI(location.pathname)=="/browserUpdate.xhtml"){
 		window.location="/auth/";
 	}
 });
