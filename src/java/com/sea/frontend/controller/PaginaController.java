06df829a233aa5390f5f274a4753a5bf5ba9ceb4
Darhakexge <darhakexge@gmail.com>
2017-04-08 11:37:13 -0500

    Menús dinámicos completados

--- src/java/com/sea/frontend/controller/PaginaController.java
+++ src/java/com/sea/frontend/controller/PaginaController.java
@@ -9,12 +9,8 @@ import com.sea.backend.entities.Pagina;
 import com.sea.backend.model.PaginaFacadeLocal;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -34,36 +30,15 @@ public class PaginaController implements Serializable {
 
 	@PostConstruct
 	public void init() {
-		try {
-			obtenerSubMenus();
-			/*try {
-			listaSubMenus = PaginaEJB.obtenerSubMenus(obtenerIdUsuario());
-			} catch (Exception ex) {
-			Logger.getLogger(PaginaController.class.getName()).log(Level.SEVERE, null, ex);
-			}*/
-		} catch (Exception ex) {
-			Logger.getLogger(PaginaController.class.getName()).log(Level.SEVERE, null, ex);
-		}
 	}
-	
-	//Obteniendo la sección enviada por parámetro
-	/*public void obtenerSeccion(){
-		FacesContext facesContext = FacesContext. getCurrentInstance();
-		ExternalContext externalContext = facesContext.getExternalContext();
-		Map params = externalContext.getRequestParameterMap();
-		seccion = (String) params.get("seccion" );
-		//System.out.println((String) faceletContext.getAttribute("seccion"));
-	}*/
 
 	//Obteniendo todos los menús del usuario
-	public void obtenerSubMenus() throws Exception {
+	public void obtenerSubMenus(String seccion) throws Exception {
 		try {
-
-			paginaEJB.create(pagina);
+			listaSubMenus = PaginaEJB.obtenerSubMenus(obtenerIdUsuario(), seccion);
 		} catch (Exception e) {
 
 		}
-		System.out.println(getSeccion());
 	}
 
 	}
@@ -75,5 +50,4 @@ public class PaginaController implements Serializable {
 	public void setSeccion(String seccion) {
 		this.seccion = seccion;
 	}
-
 }
