c718a3fe1a802f68aeb9c43cc47e5000436b32de
Darhakexge <darhakexge@gmail.com>
2017-05-04 16:57:59 -0500

    Mejorada la página de navegadores desactualizados fixed #38

--- web/resources/js/checkBrowser.js
+++ web/resources/js/checkBrowser.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 	if (!(window.CSS && window.CSS.supports && window.CSS.supports("( display: grid ) and ( --var: grid ) and ( display: var(--var) )"))) {
-		if (decodeURI(location.pathname) != "/browserUpdate.xhtml") {
+		if (decodeURI(location.pathname) != "/SEA/errorPages/browserUpdate.xhtml") {
 			window.location = "/SEA/errorPages/browserUpdate.xhtml";
 		}
-	} else if (decodeURI(location.pathname) == "/browserUpdate.xhtml") {
+	} else if (decodeURI(location.pathname) == "/SEA/errorPages/browserUpdate.xhtml") {
 		window.location = "/SEA/auth/";
 	}
 });
